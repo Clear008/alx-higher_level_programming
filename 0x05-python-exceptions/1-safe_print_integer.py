6b21cbb01438bae707f90758da6fbc50869c10c8
Clear008 <wissal-exo@hotmail.com>
2023-12-18 07:41:15 -0800

    1-safe_print_integer.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,7 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-        return value
+        return True
     except ValueError:
         print()
         return False
