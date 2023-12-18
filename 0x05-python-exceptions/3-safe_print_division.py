83755b6a31802e47c1f44e8202583127e59e0704
Clear008 <wissal-exo@hotmail.com>
2023-12-18 08:29:26 -0800

    3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -5,5 +5,5 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         res = None
     finally:
-        print("Inside result: {:.2f}".format(res))
+        print("Inside result: {}".format(res))
     return res
