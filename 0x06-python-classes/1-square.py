c05d8261a5e82193264c459acf0486e6c3ed2050
Clear008 <wissal-exo@hotmail.com>
2023-12-19 11:04:49 -0800

    0-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""my module is for defining a class Square"""
 
 
 class Square:
+    """ class for defining a square"""
     def __init__(self, size):
+        """Private instance attribute: size"""
         self.__size = size
