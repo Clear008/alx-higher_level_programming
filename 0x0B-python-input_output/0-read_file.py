efb3dcaee05d14e9c6a29106a4ce8c06c18d92b7
Clear008 <wissal-exo@hotmail.com>
2024-01-09 09:21:04 -0800

    1-write_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,5 +5,4 @@
 def read_file(filename=""):
     """Read a text file."""
     with open(filename, encoding="utf-8") as file:
-        content = file.read()
-        print(content)
+        print(file.read(), end="")
