db82d11f68e3ea14b9fa8a9b14b7560dbc09ae16
Clear008 <wissal-exo@hotmail.com>
2024-01-09 11:52:16 -0800

    12-pascal_triangle.py

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -9,9 +9,8 @@ def pascal_triangle(n):
 
     triangle = []
     for i in range(n):
-        row = [1]  
+        row = [1]
 
-        
         for j in range(1, i):
             row.append(triangle[i-1][j-1] + triangle[i-1][j])
 
@@ -21,4 +20,3 @@ def pascal_triangle(n):
         triangle.append(row)
 
     return triangle
-
