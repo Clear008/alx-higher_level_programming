c9d2590609786a02cef5c89cabda39ed47ee35ec
Clear008 <wissal-exo@hotmail.com>
2024-01-09 11:28:14 -0800

    5-save_to_json_file.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,21 +2,14 @@
 """a script that adds and save all arguments to a Python list """
 import sys
 
-
-from save_to_json_file import save_to_json_file
-from load_from_json_file import load_from_json_file
-
-
-def add_and_save_to_file(arguments):
-    try:
-
-        existing_data = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        existing_data = []
-
-    existing_data.extend(arguments)
-    save_to_json_file(existing_data, "add_item.json")
-
-
 if __name__ == "__main__":
-    add_and_save_to_file(arguments)
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
+
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
