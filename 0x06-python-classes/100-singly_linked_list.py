ad1e4887705b707715b4d6d88b07bdc92c9e4d0b
Clear008 <wissal-exo@hotmail.com>
2024-01-24 07:47:25 -0800

    100-singly_linked_list.py

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -37,7 +37,7 @@ class Node:
 
 class SinglyLinkedList:
     """Represent a node in a singly-linked list."""
-    
+
     def __init__(self):
         """Initialising the data"""
         self.__head = None
@@ -51,26 +51,24 @@ class SinglyLinkedList:
         Args:
             value (Node): The new Node to insert.
         """
-        new = Node(value)
-        if self.__head is None:
-            new.next_node = None
-            self.__head = new
-        elif self.__head.data > value:
-            new.next_node = self.__head
-            self.__head = new
-        else:
-            tmp = self.__head
-            while (tmp.next_node is not None and
-                    tmp.next_node.data < value):
-                tmp = tmp.next_node
-            new.next_node = tmp.next_node
-            tmp.next_node = new
+        new_node = Node(value)
+        if not self.head or value < self.head.data:
+            new_node.next_node = self.head
+            self.head = new_node
+            return
+
+        current = self.head
+        while current.next_node and current.next_node.data < value:
+            current = current.next_node
+
+        new_node.next_node = current.next_node
+        current.next_node = new_node
 
     def __str__(self):
-        """Define the print() representation of a SinglyLinkedList."""
-        values = []
-        tmp = self.__head
-        while tmp is not None:
-            values.append(str(tmp.data))
-            tmp = tmp.next_node
-        return ('\n'.join(values))
+        """ print the output"""
+        result = ""
+        current = self.head
+        while current:
+            result += str(current.data) + "\n"
+            current = current.next_node
+        return result
