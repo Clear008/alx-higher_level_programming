3b85830092c4d6a9c911fdc7c73fc802ed3f2f84
Clear008 <wissal-exo@hotmail.com>
2024-03-12 22:13:32 +0000

    0-rectangle.js

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,12 +1,15 @@
 #!/usr/bin/node
 
 class Rectangle {
-    constructor (w, h) {
-      this.width = w;
-      this.height = h;
-      if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
+    constructor(w, h) {
+        if (w > 0 && h > 0) {
+            this.width = w;
+            this.height = h;
+        } else {
+            this.width = 0;
+            this.height = 0;
+        }
     }
-  }
-  
-  module.exports = Rectangle;
-  
+}
+
+module.exports = Rectangle;
