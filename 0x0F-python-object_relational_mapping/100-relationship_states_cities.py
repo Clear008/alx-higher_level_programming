38e1e2c74f49ceeb6cb257729a9599562456c007
Clear008 <wissal-exo@hotmail.com>
2024-03-15 17:26:28 -0700

    100-relationship_states_cities.

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" creates the State “California” with the
+City “San Francisco” from the database """
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from model_city import City
+
+
+def main():
+    """Create cities"""
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    session.add(City(name="San Francisco", state=State(name="california")))
+    session.commit()
+
+    session.close()
+
+
+if __name__ == "__main__":
+    main()
---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" the class definition of a State """
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from relationship_city import Base, City
+
+Base = declarative_base()
+
+
+class State(Base):
+    """Represents a state for a MySQL database."""
+
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+
+    cities = relationship("City", cascade="all, delete", backref="state")
