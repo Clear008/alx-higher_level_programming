630c1a0a6d931e907f10ca8b9ab8ece5a952d081
Clear008 <wissal-exo@hotmail.com>
2024-03-15 19:04:40 -0700

    100-relationship_states_cities.

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -3,7 +3,6 @@
 
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from model_state import State, Base
 
 
 Base = declarative_base()
