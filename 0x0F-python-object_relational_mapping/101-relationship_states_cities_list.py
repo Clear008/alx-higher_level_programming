55aaa5e0f3cf84e04bee90b31fcdc7d7b432130b
Clear008 <wissal-exo@hotmail.com>
2024-03-21 18:23:22 -0700

    100-relationship_state

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
-"""List all states"""
-from sys import argv
-from relationship_state import Base, State
-from relationship_city import City
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import sessionmaker
+""" Lists all State objects, and corresponding City objects """
 
-if __name__ == "__main__":
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'
-        .format(argv[1], argv[2],
-                argv[3]), pool_pre_ping=True)
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from relationship_state import State
+from relationship_city import City
+from relationship_state import Base, State
+
+
+def main():
+    """Lists all state and cities"""
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
 
     Session = sessionmaker(bind=engine)
     session = Session()
@@ -19,3 +23,9 @@ if __name__ == "__main__":
         print("{}: {}".format(state.id, state.name))
         for city in state.cities:
             print("\t{}: {}".format(city.id, city.name))
+
+    session.close()
+
+    
+if __name__ == "__main__":
+    main()
