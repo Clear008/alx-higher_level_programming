ea2baa5b55557dcbe4cd396c319b9846e123730b
Clear008 <wissal-exo@hotmail.com>
2024-03-27 15:11:50 -0700

    6-peak

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Sends a POST request to the passed URL,displays the body of the response
-curl -s "$1" -X POST -d "email=hr@holbertonschool.com" -d "subject=I will always be here for PLD"
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
