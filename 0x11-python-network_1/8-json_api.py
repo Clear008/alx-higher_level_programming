da1affd08762069775262325bbef56b30dc68eea
Clear008 <wissal-exo@hotmail.com>
2024-03-28 18:50:19 -0700

    8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""akes in a letter and sends a POST request to http://0.0.0.0:5000/search_user."""
+"""Makes a POST request to http://0.0.0.0:5000/search_user."""
 
 import requests
 from sys import argv
 
 if __name__ == "__main__":
-    l = "" if len(sys.argv) == 1 else sys.argv[1]
-    rt = requests.post('http://0.0.0.0:5000/search_user', data={'q': l})
+    query = "" if len(argv) == 1 else argv[1]
+    rt = requests.post('http://0.0.0.0:5000/search_user', data={'q': query})
     try:
         res = rt.json()
         if res == {}:
