5816ad2d02ad3d0a62c29788cfe2c662228e903d
Clear008 <wissal-exo@hotmail.com>
2024-03-28 18:01:53 -0700

    1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,9 +3,10 @@
     the value of the X-Request-Id
 """
 
-import sys
-import urllib.request
+if __name__ == "__main__":
+    import sys
+    import urllib.request
 
-with urllib.request.urlopen(sys.argv[1]) as response:
-    head = response.headers.get('X-Request-Id')
-    print(head)
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        head = response.headers.get('X-Request-Id')
+        print(head)
