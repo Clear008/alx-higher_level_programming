ed5f8de88a916d654473a024edc66d86129ab563
Clear008 <wissal-exo@hotmail.com>
2024-03-28 18:53:39 -0700

    8-json_api.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
-"""akes your GitHub credentials (username and password) and uses
-    the GitHub API to display your id.
-"""
+"""Takes in GitHub credentials (username and password) and uses the GitHub API
+to display an id"""
 
 import requests
 import sys
-from requests.auth import HTTPBasicAuth
-
 
 if __name__ == "__main__":
-    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
-    r = requests.get("https://api.github.com/user", auth=auth)
-    print(r.json().get("id"))
+    if len(sys.argv) != 3:
+        print("Usage: {} <username> <password>".format(sys.argv[0]))
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+
+    response = requests.get('https://api.github.com/user',
+                            auth=(username, password))
+
+    if response.status_code == 200:
+        user_info = response.json()
+        print(user_info.get('id'))
+    else:
+        print('None')
